40b17e179612a79915ed7a35eb7f6ddb1a0eb939
Assanfakou <fakouhasan@gmail.com>
2026-07-07 17:13:15 +0100

    colors  header

---
+++ include/colors.h
@@ -0,0 +1,14 @@
+#ifndef COLORS_H
+#define COLORS_H
+
+#define Black "\033[0;30m"        // Black
+#define RED "\033[0;31m"         // Red
+#define Green "\033[0;32m"        // Green
+#define Yellow "\033[0;33m"       // Yellow
+#define Blue "\033[0;34m"         // Blue
+#define Purple "\034[0;35m"       // Purple
+#define Cyan "\033[0;36m"         // Cyan
+#define White "\033[0;37m"        // White
+#define RESET "\033[0m" // reset 
+
+#endif
