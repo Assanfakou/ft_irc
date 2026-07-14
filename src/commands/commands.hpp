c85bb40ec1d32ed70dc48e4065b96f47dd08477f
Assanfakou <fakouhasan@gmail.com>
2026-07-14 08:40:22 +0100

    to complete now: refactore the messages to the class reply

--- src/commands/commands.hpp
+++ src/commands/commands.hpp
@@ -1,3 +1,4 @@
 #include "../../include/Message.hpp"
 
 void privmsg(Server &server, Client &client, const Message &msg);
+void notice(Server &server, Client &client, const Message &msg);
--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -1,10 +1,19 @@
 #include "commands.hpp"
+/*
+** Privmsg command implementation here 
+**
+** Server &server: The server instance
+** Client &sender: The client who sent the PRIVMSG command
+** const Message &msg: The parsed message containing the command and parameters
+**
+** It stil needd to implement the #chanel users.
+*/
 
 void privmsg(Server &server, Client &sender, const Message &msg)
 {
     if (msg.getParams().empty())
     {
-        std::string errorMessage = ":Server 411 " + sender.getNickname() + " " + msg.getParameter(0) + ": ERR_NORECIPIENT\r\n";
+        std::string errorMessage = ":Server 461 " + sender.getNickname() + " : ERR_NEEDMOREPARAMS\r\n";
         server.sendMessageToClient(sender.getFd(), errorMessage);
         return;
     }
@@ -44,5 +53,48 @@ void privmsg(Server &server, Client &sender, const Message &msg)
             std::string errorMessage = ":Server 401 : ERR_NOSUCHNICK\r\n";
             server.sendMessageToClient(sender.getFd(), errorMessage);
             return ;
+    }
+}
+
+void notice(Server &server, Client &sender, const Message &msg)
+{
+    if (msg.getParams().empty())
+        return;
+    std::string receiver = msg.getParameter(0);
+    if (receiver.empty())
+        return;
+    if (msg.getParams().size() < 2)
+        return;
+    std::string message = msg.getParameter(1);
+    std::vector<Client *> receiverClients = server.getClientsByNickname(receiver);
+    if (!receiverClients.empty())
+    {
+        for (std::vector<Client *>::iterator it = receiverClients.begin(); it != receiverClients.end(); ++it)
+        {
+            Client *receiverClient = *it;
+            if (receiverClient->getFd() == sender.getFd())
+                continue;;
+            std::string fullMessage = ":" + sender.getPrefix() + " NOTICE " + receiverClient->getNickname() + " :" + message + "\r\n";
+            server.sendMessageToClient(receiverClient->getFd(), fullMessage);
+        }
+        return ;
+    }
+    else
+        return;
+}
+void whois(Server &server, Client &sender, const Message &msg)
+{
+    if (msg.getParams().empty())
+    {
+        std::string errorMessage = ":Server 461 " + sender.getNickname() + " : ERR_NEEDMOREPARAMS\r\n";
+        server.sendMessageToClient(sender.getFd(), errorMessage);
+        return;
+    }
+    std::string targetNickname = msg.getParameter(0);
+    if (!targetNickname.empty())
+    {
+        Client *receiverClient = server.getClientByNickname(targetNickname);
+        std::string messageToSend = ":" + sender.getPrefix() + " WHO ";
+        server.sendMessageToClient(receiverClient->getFd(), messageToSend);
     }
 }
