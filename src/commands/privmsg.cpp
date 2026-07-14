3118e9926c2b202ad886584c041344918b1cec1c
Assanfakou <fakouhasan@gmail.com>
2026-07-14 01:54:35 +0100

    error handling

--- src/commands/privmsg.cpp
+++ src/commands/privmsg.cpp
@@ -3,20 +3,22 @@
 void privmsg(Server &server, Client &sender, const Message &msg)
 {
     (void) sender;  
+    std::string reciever = msg.getParameter(0);
     if (msg.getParams().size() < 2)
     {
         if (server.getClientByNickname(msg.getParameter(0)))
         {
-            std::cout << RED << "no message to send\n" << RESET;
+            std::string errorMessage = ":server 400 : No text to send\r\n";
+            server.sendMessageToClient(sender.getFd(), errorMessage);
             return ;
         }
         else
         {
-            std::cout << RED << "no Reciever mentioned \n" << RESET;
+            std::string errorMessage = ":Server 401 " + sender.getNickname() + " " + msg.getParameter(0) + ": No nick/chanel\r\n";
+            server.sendMessageToClient(sender.getFd(), errorMessage);
             return ;
         }
     }
-    std::string reciever = msg.getParameter(0);
     std::string message = msg.getParameter(1);
     Client *receiverClient = server.getClientByNickname(reciever);
     if (receiverClient)
@@ -28,8 +30,9 @@ void privmsg(Server &server, Client &sender, const Message &msg)
     }
     else
     {
-        std::cout << RED << "Reciever not found\n" << RESET;
-        return ;
+            std::string errorMessage = ":server 401 : No Reciver\r\n";
+            server.sendMessageToClient(sender.getFd(), errorMessage);
+            return ;
     }
     std::cout << Blue << "Everything is good\n" << RESET;
 }
